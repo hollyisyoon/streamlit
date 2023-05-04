721e32a1b2702470a8f9085a3d82fa8554e63de1
hollyisyoon <126076416+hollyisyoon@users.noreply.github.com>
2023-05-05 01:59:24 +0900

    .

--- pages/연습장.py
+++ pages/연습장.py
@@ -2,12 +2,12 @@ from datetime import date
 
 min_date = date(2022, 7, 25)
 max_date = date(2023, 4, 26)
-with col1:
-    # 시작 날짜와 끝 날짜를 동시에 입력받음
-    start_end_date = st.date_input("시작 날짜 - 끝 날짜",
-                                   value=(date(2023,4,5), date(2023,4,20)),
-                                   min_value=min_date + timedelta(days=7),
-                                   max_value=max_date - timedelta(days=90),
-                                   key="date_range")
-    start_date = start_end_date[0]
-    end_date = start_end_date[1]
+
+# 시작 날짜와 끝 날짜를 동시에 입력받음
+start_end_date = st.date_input("시작 날짜 - 끝 날짜",
+                                value=(date(2023,4,5), date(2023,4,20)),
+                                min_value=min_date + timedelta(days=7),
+                                max_value=max_date - timedelta(days=90),
+                                key="date_range")
+start_date = start_end_date[0]
+end_date = start_end_date[1]
