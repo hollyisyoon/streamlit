f92ad69e06b738ed12fa54dc4a10615fbb160403
hollyisyoon <126076416+hollyisyoon@users.noreply.github.com>
2023-05-05 02:06:37 +0900

    .

--- pages/새로운 마음가짐.py
+++ pages/새로운 마음가짐.py
@@ -57,17 +57,17 @@ st.title('외부 트렌드 모니터링 대시보드')
 #### 인풋 필터 #####
 col1, col2, col3 = st.beta_columns(3)
 
-min_date = date(2022, 7, 25)
-max_date = date(2023, 4, 26)
+min_date = datetime(2022, 7, 25)
+max_date = datetime(2023, 4, 26)
 with col1:
 # 시작 날짜와 끝 날짜를 따로 입력받음
     start_date = st.date_input("시작 날짜",
-                                value=date(2023,4,5),
+                                value=datetime(2023,4,5),
                                 min_value=min_date,
                                 max_value=max_date - timedelta(days=90),
                                 key="start_date")
     end_date = st.date_input("끝 날짜",
-                                value=date(2023,4,20),
+                                value=datetime(2023,4,20),
                                 min_value=start_date + timedelta(days=7),
                                 max_value=max_date,
                                 key="end_date")
