9539308780d8832be74fd6afb8f4e5b8fffffa72
hollyisyoon <126076416+hollyisyoon@users.noreply.github.com>
2023-05-05 16:07:59 +0900

    d

--- pages/연습장.py
+++ pages/연습장.py
@@ -8,16 +8,15 @@ df = pd.DataFrame({'키워드':['참', '걸'], '평균 영향도':[0.559585, 0.4
 # 키워드와 평균 영향도를 annoted_text로 변환하는 함수
 def format_keyword_score(row):
     keyword = row['키워드']
-    score = row['평균 영향도']
-    text = f"{keyword} ({score:.3f})"
-    return text
+    return keyword
 
 # 각 행을 annotated_text로 변환
 texts = []
 for i, row in df.iterrows():
     keyword_score_text = format_keyword_score(row)
-    url = row['URL']
-    texts.append((keyword_score_text, url))
+    score = row['평균 영향도']
+    score = f'{score:.3f}'
+    texts.append((keyword_score_text, score))
 
 # annotated_text 출력
 annotated_text(*texts)
