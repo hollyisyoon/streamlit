1cc53032aa243d6ff01bac976c724dd46b9b57c4
hollyisyoon <126076416+hollyisyoon@users.noreply.github.com>
2023-05-05 16:19:38 +0900

    d

--- pages/연습장.py
+++ pages/연습장.py
@@ -1,3 +1,12 @@
+
+링크를 클릭 가능한 형태로 변경해주기 위해서 annotated_text() 함수의 mark_text() 메소드를 사용하여 텍스트에 앵커 태그(anchor tag)를 추가하면 됩니다. 이때 앵커 태그의 href 속성을 링크 URL로 지정해주면 됩니다.
+
+그리고 호버 이벤트를 처리하기 위해서는 annotated_text() 함수의 add_style() 메소드를 사용하여 CSS 스타일을 추가해주어야 합니다. 이때 CSS 스타일의 cursor 속성을 pointer로 설정하면 마우스 커서가 포인터 모양으로 변경되어 클릭 가능한 링크임을 사용자에게 알릴 수 있습니다.
+
+아래는 수정된 코드 예시입니다.
+
+python
+Copy code
 import streamlit as st
 from annotated_text import annotated_text
 
@@ -19,15 +28,4 @@ for i, row in df.iterrows():
     link = row['URL']
     texts.append((keyword_score_text, score, link))
 
-# annotated_text 출력
-for text in texts:
-    keyword_score_text = text[0] + ' (' + text[1] + ')'
-    link = text[2]
-    annotated_text(
-        (keyword_score_text, None),
-        (f'[{link}]', link),
-        add_spaces=True
-    ).add_style(
-        f'a:hover',
-        cursor='pointer',
-    )
+st.write(texts)
