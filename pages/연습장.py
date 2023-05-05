78d6aa4aac87e7c8b2fba9320c0c079a9b7f9d75
hollyisyoon <126076416+hollyisyoon@users.noreply.github.com>
2023-05-05 17:53:38 +0900

    ,

--- pages/연습장.py
+++ pages/연습장.py
@@ -16,16 +16,58 @@ from markdownlit import mdlit
 
 # 데이터프레임 생성
 import pandas as pd
+import itertools
 
+df_concat_github = pd.read_csv('https://raw.githubusercontent.com/hollyisyoon/streamlit/main/data/df_%E1%84%90%E1%85%B3%E1%84%85%E1%85%A6%E1%86%AB%E1%84%83%E1%85%B3_github.csv')
 
-df = pd.DataFrame({'키워드':['참', '걸'], '평균 영향도':[0.559585, 0.476684], 'URL':['https://band.us/band/86294308/post/322', 'https://band.us/band/86294308/post/358']})
+df_concat_github['날짜'] = pd.to_datetime(df_concat_github['날짜'])
+
+def extract_df(df, media, start_date, end_date, effect_size):
+    start_date = pd.Timestamp(start_date)
+    end_date = pd.Timestamp(end_date)
+    standard_df = df[(df['매체'] == media) & (df['날짜'] >= start_date) & (df['날짜'] <= end_date) & (df['영향도'] >= effect_size)]
+    range_days = (end_date - start_date) + timedelta(days = 1)
+    new_day = start_date - range_days
+    new_day = pd.Timestamp(new_day)
+    new_df = df[(df['매체'] == media) & (df['날짜'] >= new_day) & (df['날짜'] < start_date) & (df['영향도'] >= effect_size)]
+    
+    return standard_df, new_df
+
+standard_df, new_df = extract_df(df_concat_github, '식물갤러리', df_concat_github['날짜'][3000], df_concat_github['날짜'][0], 0)
 
 def convert_to_markdown(row):
     return f"[`{row['키워드']} | {row['평균 영향도']:.6f}`]({row['URL']})"
 
-markdown_rows = df.apply(convert_to_markdown, axis=1).tolist()
-markdown_text = '   '.join(markdown_rows)
-mdlit(f"""{markdown_text}""")
+def make_keyword_tag(df):
+    markdown_rows = df.apply(convert_to_markdown, axis=1).tolist()
+    markdown_text = '   '.join(markdown_rows)
+    return mdlit(f"""{markdown_text}""")
+
+def new_keyword(standard_df, new_df):
+    # 각각의 데이터프레임에서 title+content 칼럼을 추출하여 리스트로 변환
+    content_list_1 = []
+    content_list_1.extend(list(itertools.chain.from_iterable([eval(i) for i in standard_df['제목+내용(nng)']])))
+    content_list_2 = []
+    content_list_2.extend(list(itertools.chain.from_iterable([eval(i) for i in new_df['제목+내용(nng)']])))
+
+    new_keywords = set(content_list_2) - set(content_list_1)   
+    result_dict = {}
+
+    # 이번달에만 있는 
+    for word in new_keywords:
+        word_df = new_df[new_df['제목+내용(nng)'].str.contains(word)]
+        if len(word_df) > 0:
+            avg_views = word_df['영향도'].mean()
+            urls = word_df['URL'].tolist()
+            result_dict[word] = {'평균 영향도': float(avg_views), 'URL': urls}
+            
+    # 조회수 높은순으로 정렬        
+    result_dict = dict(sorted(result_dict.items(), key=lambda item: item[1]['평균 영향도'], reverse=True))    
+    return result_dict
+
+new_keyword = pd.DataFrame(new_keyword(standard_df, new_df), columns=['키워드', '평균 영향도', 'URL'])
+make_keyword_tag(new_keyword)
+
 
 
 
