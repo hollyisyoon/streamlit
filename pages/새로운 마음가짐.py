83da2069ea66681f5cf9b8bb4d5f4edc4b77ec00
hollyisyoon <126076416+hollyisyoon@users.noreply.github.com>
2023-05-06 01:49:26 +0900

    .

--- pages/새로운 마음가짐.py
+++ pages/새로운 마음가짐.py
@@ -155,6 +155,9 @@ with col2:
 def convert_to_markdown(row):
     return f"[`{row['키워드']} | {row['평균 영향도']*100:.0f}`]({row['URL']})"
 
+def convert_to_markdown2(row):
+    return f"[`{row['키워드']} | {row['상승률']*100:.0f}`]({row['URL']})"
+
 def make_keyword_tag(df):
     markdown_rows = df.apply(convert_to_markdown, axis=1).tolist()
     markdown_text = '   '.join(markdown_rows)
@@ -198,9 +201,69 @@ def new_keyword(standard_df, new_df):
 
     return result_df
 
+def rising_keyword(standard_df, new_df):
+    # 데이터 합치기 
+    df = pd.concat([standard_df, new_df])
+    
+    # 날짜 구하기
+    이번주마지막날 = df['날짜'].max()
+    이번주첫날 = (df['날짜'].max() - timedelta(days=7))
+    지난주첫날 = 이번주첫날 - timedelta(days=7)
+    
+    이번주_df = df[(df['날짜'] > 이번주첫날) & (df['날짜'] <= 이번주마지막날)]
+    지난주_df = df[(df['날짜'] > 지난주첫날) & (df['날짜'] <= 이번주첫날)]
+        
+    # 중복값 제거한 새로운 열 추가
+    이번주_df = 이번주_df.copy()
+    이번주_df['unique_content'] = 이번주_df['제목+내용(nng)'].apply(lambda x: ast.literal_eval(x))
+    이번주_df['unique_content'] = 이번주_df['unique_content'].apply(lambda x: list(set(x)))
+
+    지난주_df = 지난주_df.copy()
+    지난주_df['unique_content'] = 지난주_df['제목+내용(nng)'].apply(lambda x: ast.literal_eval(x))
+    지난주_df['unique_content'] = 지난주_df['unique_content'].apply(lambda x: list(set(x)))
+
+    this_week_words = list(이번주_df['unique_content'].explode())
+    last_week_words = list(지난주_df['unique_content'].explode())
+
+    this_week_word_counts = Counter(this_week_words)
+    last_week_word_counts = Counter(last_week_words)
+
+    # 이번주와 지난주에 모두 언급된 단어를 모은 집합
+    common_words = set(this_week_word_counts.keys()) & set(last_week_word_counts.keys())
+
+    result = {}
+    for word in common_words:
+        # 해당 단어가 언급된 모든 URL을 리스트로 모음
+        url_list = list(이번주_df.loc[이번주_df['unique_content'].apply(lambda x: word in x)]['URL'])
+        # 영향도가 가장 높은 URL을 찾아서 출력
+        url = max(url_list, key=lambda x: 이번주_df.loc[이번주_df['URL'] == x, '영향도'].iloc[0])
+        increase_rate = (this_week_word_counts[word] - last_week_word_counts[word]) / this_week_word_counts[word]
+        result[word] = {'상승률': round(increase_rate*100, 2), 'URL': url}
+    
+    # 상승률 기준 상위 10개 단어 출력
+    keywords = []
+    ups = []
+    urls = []
+
+    for word, data in sorted(result.items(), key=lambda x: x[1]['상승률'], reverse=True):
+        if data['상승률']>0:
+        keywords.append(word)
+        ups.append(f"{data['상승률']}%")
+        urls.append(data['URL'])
+
+    result_df = pd.DataFrame({
+        '키워드': keywords,
+        '상승률': ups,
+        'URL': urls
+    })
+
+    return result_df
+
 ### 키워드 ###
 st.title('신규 키워드 ✨')
 new_keyword = new_keyword(standard_df, new_df)
 make_keyword_tag(new_keyword)
 
 st.title('🔥 급상승 키워드')
+rising_keyword = rising_keyword(standard_df, new_df)
+make_keyword_tag(rising_keyword)
