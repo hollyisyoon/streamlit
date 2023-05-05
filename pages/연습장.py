25b4d0b8fa312361caf3e10454324ce261cb6521
hollyisyoon <126076416+hollyisyoon@users.noreply.github.com>
2023-05-05 16:04:56 +0900

    ㅇ

--- pages/연습장.py
+++ pages/연습장.py
@@ -1,17 +1,23 @@
 import streamlit as st
 from annotated_text import annotated_text
 
-annotated_text(
-    "This ",
-    ("일장", "1"),
-    " some ",
-    ("annotated", "adj"),
-    ("text", "noun"),
-    " for those of ",
-    ("you", "pronoun"),
-    " who ",
-    ("like", "verb"),
-    " this sort of ",
-    ("thing", "noun"),
-    "."
-)
+# 데이터프레임 생성
+import pandas as pd
+df = pd.DataFrame({'키워드':['참', '걸'], '평균 영향도':[0.559585, 0.476684], 'URL':['https://band.us/band/86294308/post/322', 'https://band.us/band/86294308/post/358']})
+
+# 키워드와 평균 영향도를 annoted_text로 변환하는 함수
+def format_keyword_score(row):
+    keyword = row['키워드']
+    score = row['평균 영향도']
+    text = f"{keyword} ({score:.3f})"
+    return text
+
+# 각 행을 annotated_text로 변환
+texts = []
+for i, row in df.iterrows():
+    keyword_score_text = format_keyword_score(row)
+    url = row['URL']
+    texts.append((keyword_score_text, url))
+
+# annotated_text 출력
+annotated_text(*texts)
