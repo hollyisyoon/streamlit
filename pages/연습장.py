63828f4c1e2ceba1ed06657fc65d5a99acc598d6
hollyisyoon <126076416+hollyisyoon@users.noreply.github.com>
2023-05-05 17:25:26 +0900

    ㄱㄷㅇ

--- pages/연습장.py
+++ pages/연습장.py
@@ -26,16 +26,7 @@ keyword_color = 'red'
 
 # markdownlit으로 변환
 mdlit(
-    f"""[`윤훈영 | {keyword_color} keyword`](https://hollyyoon.oopy.io) [`윤훈영 | {keyword_color} keyword`](https://hollyyoon.oopy.io) 
-    Tired from [default links](https://extras.streamlit.app)?
-Me too! Discover Markdownlit's `@()` operator. Just insert a link and it
-will figure a nice icon and label for you!
-Example: @(https://extras.streamlit.app)... better, right? You can
-also @(🍐)(manually set the label if you want)(https://extras.streamlit.app)
-btw, and play with a [red]beautiful[/red] [blue]set[/blue] [orange]of[/orange]
-[violet]colors[/violet]. Another perk is those beautiful arrows -> <-
-
-"""
+    f"""[`윤훈영 | {{color {keyword_color}}} keyword`](https://hollyyoon.oopy.io)"""
 )
 
 
