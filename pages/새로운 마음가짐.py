926b5f4e41f8bc53f08090f0d673b69043b37b4d
hollyisyoon <126076416+hollyisyoon@users.noreply.github.com>
2023-05-06 14:13:45 +0900

    .

--- pages/새로운 마음가짐.py
+++ pages/새로운 마음가짐.py
@@ -343,5 +343,6 @@ def plot_keyword_impact_grey(df, keywords):
     fig.update_layout(title_text="시간별 키워드 영향도", xaxis_title="날짜", yaxis_title="평균 영향도")
     st.plotly_chart(fig, use_container_width=True)
 
-deepdive_df, keyword_list = get_df(df, keyword1, keyword2)
-plot_keyword_impact_grey(deepdive_df, keyword_list)
+keyword2
+get_df(df, keyword1, keyword2)
+# plot_keyword_impact_grey(deepdive_df, keyword_list)
