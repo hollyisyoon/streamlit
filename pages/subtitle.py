f0c1aa2d27f8fdd3e13789184c4b2c27c6e565e8
hollyisyoon <126076416+hollyisyoon@users.noreply.github.com>
2023-05-07 14:39:15 +0900

    .

--- pages/subtitle.py
+++ pages/subtitle.py
@@ -1,4 +1,18 @@
 import streamlit as st
+from streamlit import components
+
+# JavaScript 함수 정의 (클릭 이벤트 처리)
+js_code = """
+<script>
+    function scrollToContent(sectionId) {
+        const element = document.getElementById(sectionId);
+        element.scrollIntoView({ behavior: 'smooth' });
+    }
+</script>
+"""
+
+# 컴포넌트로 HTML 코드 추가
+components.html(js_code)
 
 # 사이드바에 HTML을 추가하여 서브타이틀 클릭 이벤트 처리
 st.sidebar.markdown("""
@@ -16,13 +30,3 @@ st.write("여기에 서브타이틀 2의 내용을 작성합니다.")
 
 st.markdown("<h1 id='section3'>서브타이틀 3 내용</h1>", unsafe_allow_html=True)
 st.write("여기에 서브타이틀 3의 내용을 작성합니다.")
-
-# JavaScript 함수 정의 (클릭 이벤트 처리)
-st.markdown("""
-    <script>
-        function scrollToContent(sectionId) {
-            const element = document.getElementById(sectionId);
-            element.scrollIntoView({ behavior: 'smooth' });
-        }
-    </script>
-""", unsafe_allow_html=True)
