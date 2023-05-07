743e265596a3c09b4473e1a67085a5a08676bf76
hollyisyoon <126076416+hollyisyoon@users.noreply.github.com>
2023-05-07 13:17:26 +0900

    .

--- pages/새로운 마음가짐.py
+++ pages/새로운 마음가짐.py
@@ -115,7 +115,7 @@ with col2:
     media = st.selectbox('매체',('식물갤러리', '식물병원', '네이버카페', '네이버블로그', '네이버포스트'))
 
 with col3:
-    temp_effect_size = st.slider('영향도 볼륨', 0, 100, 83)
+    temp_effect_size = st.slider('영향도 볼륨', 0, 100, 83, help="영향도 볼륨이란 각 매체별 콘텐츠의 반응도를 점수화한 값입니다. 0에 가까울 수록 영향도가 높습니다.")
     effect_size = (100-int(temp_effect_size))/100
 
 standard_df, new_df = extract_df(df, media, start_date, end_date, effect_size)
