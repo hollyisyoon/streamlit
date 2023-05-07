5fb98c8089132ecc632c78308ebbab008eef8f7a
moru_xz <120144146+kimxz79@users.noreply.github.com>
2023-05-07 22:56:40 +0900

    d

--- pages/효정.py
+++ pages/효정.py
@@ -95,6 +95,7 @@ def deepdive_lineplot(df, keywords):
                 name=keyword,
                 line_color=colors[i],
                 line=dict(dash='dot') if len(interpolated_idx) > 0 else None
+                mode = 'lines+markers'
             ),
             secondary_y=False
         )
