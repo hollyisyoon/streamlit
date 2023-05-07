23a3076100acb2f86948768d2e1a1383cc476f93
hollyisyoon <126076416+hollyisyoon@users.noreply.github.com>
2023-05-07 14:54:33 +0900

    .

--- pages/subtitle.py
+++ pages/subtitle.py
@@ -17,11 +17,10 @@ js_code = """
 # 컴포넌트로 HTML 코드 추가
 components.html(js_code)
 
-# 사이드바에 HTML을 추가하여 서브타이틀 클릭 이벤트 처리
 st.sidebar.markdown("""
-    <h2 onclick="scrollToContent('section1')">서브타이틀 1</h2>
-    <h2 onclick="scrollToContent('section2')">서브타이틀 2</h2>
-    <h2 onclick="scrollToContent('section3')">서브타이틀 3</h2>
+    <a href="javascript:void(0);" onclick="scrollToContent('section1')">서브타이틀 1</a>
+    <a href="javascript:void(0);" onclick="scrollToContent('section2')">서브타이틀 2</a>
+    <a href="javascript:void(0);" onclick="scrollToContent('section3')">서브타이틀 3</a>
 """, unsafe_allow_html=True)
 
 # 메인 콘텐츠 영역
