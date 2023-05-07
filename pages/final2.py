3521ca6e20a6c7c8ab9f91e8fe95f203f6591a99
hollyisyoon <126076416+hollyisyoon@users.noreply.github.com>
2023-05-07 19:34:31 +0900

    .

--- pages/final2.py
+++ pages/final2.py
@@ -239,14 +239,15 @@ def 네트워크(network_list, all_keywords):
     return [net, similar_words]
 
 #연관분석
+network_list = [eval(i) for i in df2['제목+내용(nng)']]
+네트워크 = 네트워크(network_list, all_keywords)
 if st.button('분석을 시작하기'):
     with st.spinner('분석 중입니다...'):
-        network_list = [eval(i) for i in df2['제목+내용(nng)']]
-        네트워크 = 네트워크(network_list, all_keywords)
-        try:
-            net = 네트워크[0]
-            net.save_graph('/app/streamlit/pyvis_graph.html')
-            HtmlFile = open('/app/streamlit/pyvis_graph.html', 'r', encoding='utf-8')
-            components.html(HtmlFile.read(), height=600)
-        except:
-            st.warning('존재하지 않는 키워드예요.')
+    try:
+        net = 네트워크[0]
+        net.save_graph('/app/streamlit/pyvis_graph.html')
+        HtmlFile = open('/app/streamlit/pyvis_graph.html', 'r', encoding='utf-8')
+        components.html(HtmlFile.read(), height=600)
+    except:
+        st.warning('존재하지 않는 키워드예요.')
+            
