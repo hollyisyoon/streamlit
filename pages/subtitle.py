0d62451e78fd03529c086d6902c2882b987a11f9
hollyisyoon <126076416+hollyisyoon@users.noreply.github.com>
2023-05-07 14:55:46 +0900

    .

--- pages/subtitle.py
+++ pages/subtitle.py
@@ -1,34 +1,35 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-# JavaScript 함수 정의 (클릭 이벤트 처리)
-js_code = """
-<script>
-    window.onload = function() {
-        function scrollToContent(sectionId) {
-            const element = document.getElementById(sectionId);
-            element.scrollIntoView({ behavior: 'smooth' });
-        }
+# CSS 스타일 정의
+css_code = """
+<style>
+    a.custom-link {
+        display: block;
+        font-size: 24px;
+        font-weight: bold;
+        color: #333;
+        text-decoration: none;
     }
-</script>
+</style>
 """
 
+# 컴포넌트로 CSS 코드 추가
+components.html(css_code)
 
-# 컴포넌트로 HTML 코드 추가
-components.html(js_code)
-
+# 사이드바에 HTML을 추가하여 서브타이틀 클릭 이벤트 처리
 st.sidebar.markdown("""
-    <a href="javascript:void(0);" onclick="scrollToContent('section1')">서브타이틀 1</a>
-    <a href="javascript:void(0);" onclick="scrollToContent('section2')">서브타이틀 2</a>
-    <a href="javascript:void(0);" onclick="scrollToContent('section3')">서브타이틀 3</a>
+    <a class="custom-link" href="#section1">서브타이틀 1</a>
+    <a class="custom-link" href="#section2">서브타이틀 2</a>
+    <a class="custom-link" href="#section3">서브타이틀 3</a>
 """, unsafe_allow_html=True)
 
 # 메인 콘텐츠 영역
-st.markdown("<h1 id='section1'>서브타이틀 1 내용</h1>", unsafe_allow_html=True)
+st.markdown("<h2 id='section1'>서브타이틀 1 내용</h2>", unsafe_allow_html=True)
 st.write("여기에 서브타이틀 1의 내용을 작성합니다.")
 
-st.markdown("<h1 id='section2'>서브타이틀 2 내용</h1>", unsafe_allow_html=True)
+st.markdown("<h2 id='section2'>서브타이틀 2 내용</h2>", unsafe_allow_html=True)
 st.write("여기에 서브타이틀 2의 내용을 작성합니다.")
 
-st.markdown("<h1 id='section3'>서브타이틀 3 내용</h1>", unsafe_allow_html=True)
+st.markdown("<h2 id='section3'>서브타이틀 3 내용</h2>", unsafe_allow_html=True)
 st.write("여기에 서브타이틀 3의 내용을 작성합니다.")
