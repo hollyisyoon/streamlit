a4aae333ffde7b267eab1eab6c7a7476dacee3b5
moru_xz <120144146+kimxz79@users.noreply.github.com>
2023-05-07 22:45:41 +0900

    ㄴ

--- pages/효정.py
+++ pages/효정.py
@@ -97,7 +97,8 @@ def deepdive_lineplot(df, keywords):
             ),
             secondary_y=False
         )
-        
+    fig.add_trace(go.Scatter(x=impact.index, y=impact.values, name=keyword, line_color=color, line=dict(dash='dot')), secondary_y=False)
+    
     fig.update_layout(yaxis_title="평균 영향도")
     st.plotly_chart(fig, use_container_width=True)
 
