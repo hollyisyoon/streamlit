5f021a620f3c841a47dd9323816c9e745126950a
hollyisyoon <126076416+hollyisyoon@users.noreply.github.com>
2023-05-08 03:04:46 +0900

    .

--- pages/연습.py
+++ pages/연습.py
@@ -2,27 +2,26 @@ import streamlit as st
 
 html_code = '''
 <div class="team-member">
-  <div class="member-info">
-    <h3 class="name">윤훈영</h3>
-    <p class="introduction">안녕하세요오오옹.</p>
-  </div>
-  <img src="https://media.licdn.com/dms/image/D5603AQGLWfWNmVBIYQ/profile-displayphoto-shrink_800_800/0/1665667362702?e=1689206400&v=beta&t=2RzA1JP0qxRbKImCayGJqEMuFZwZqbTR8QYGLAyz5Rg" alt="Profile Image" class="profile-image">
+    <img src="https://media.licdn.com/dms/image/D5603AQGLWfWNmVBIYQ/profile-displayphoto-shrink_800_800/0/1665667362702?e=1689206400&v=beta&t=2RzA1JP0qxRbKImCayGJqEMuFZwZqbTR8QYGLAyz5Rg" alt="Profile Image" class="profile-image">
+    <div class="member-info">
+        <h3 class="name">윤훈영</h3>
+        <p class="introduction">안녕하세요오오옹.</p>
+    </div>
 </div>
 
-<div class="team-member">
-  <div class="member-info">
-    <h3 class="name">Jane Smith</h3>
-    <p class="introduction">Praesent accumsan ligula nec mauris tincidunt, ac volutpat ex condimentum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.</p>
-  </div>
-  <img src="profile_image.jpg" alt="Profile Image" class="profile-image">
-</div>
+# <div class="team-member">
+#   <div class="member-info">
+#     <h3 class="name">Jane Smith</h3>
+#     <p class="introduction">Praesent accumsan ligula nec mauris tincidunt, ac volutpat ex condimentum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.</p>
+#   </div>
+#   <img src="profile_image.jpg" alt="Profile Image" class="profile-image">
+# </div>
 '''
 
 css_code = '''
 <style>
 .team-member {
   display: flex;
-  align-items: center;
   margin-bottom: 20px;
 }
 
