e3eadde57c85a7ee25c84f74ca8731150fa4c02a
hollyisyoon <126076416+hollyisyoon@users.noreply.github.com>
2023-05-07 17:02:37 +0900

    .

--- pages/final2.py
+++ pages/final2.py
@@ -243,11 +243,10 @@ if st.button('분석을 시작하기'):
     with st.spinner('분석 중입니다...'):
         network_list = [eval(i) for i in df2['제목+내용(nng)']]
         네트워크 = 네트워크(network_list, all_keywords)
-        all_keywords
-        # try:
-        #     net = 네트워크[0]
-        #     net.save_graph('/app/streamlit/pyvis_graph.html')
-        #     HtmlFile = open('/app/streamlit/pyvis_graph.html', 'r', encoding='utf-8')
-        #     components.html(HtmlFile.read(), height=600)
-        # except:
-        #     st.warning('존재하지 않는 키워드예요.')
+        try:
+            net = 네트워크[0]
+            net.save_graph('/app/streamlit/pyvis_graph.html')
+            HtmlFile = open('/app/streamlit/pyvis_graph.html', 'r', encoding='utf-8')
+            components.html(HtmlFile.read(), height=600)
+        except:
+            st.warning('존재하지 않는 키워드예요.')
